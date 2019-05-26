8f480af488e3f06f59f38aace4dde692bfd4937e
ArcticCrow <arctic-crow@web.de>
2019-05-26 20:15:54 +0200

    Updated BrowserController with exit automation

--- src/com/cloudwhale/browsercontroller/Main.java
+++ src/com/cloudwhale/browsercontroller/Main.java
@@ -8,39 +8,6 @@ public class Main {
     private static  final String LAUNCHER_URL = "https://cloudwhale.nl/launcher";
 
     public static void main(String[] args) throws InterruptedException {
-        SeleniumBrowserController controller = new SeleniumBrowserController();
-
-        controller.openUrl(LAUNCHER_URL);
-
-        boolean titleMatchesHa = controller.titleMatches("ha!");
-        boolean titleMatches2ButtonLauncher = controller.titleMatches("Unity WebGL Player | 2ButtonLauncher");
-
-        System.out.println("Title matches 'ha!' = " + titleMatchesHa);
-        System.out.println("Title matches 'Unity WebGL Player | 2ButtonLauncher' = " + titleMatches2ButtonLauncher);
-
-        Thread.sleep(2000);
-
-        controller.quit();
+        java.awt.EventQueue.invokeLater(() -> new SeleniumBrowserController(LAUNCHER_URL).setVisible(true));
     }
-
-    /*
-    public static void main(String[] args) throws InterruptedException {
-        BrowserController controller = new BrowserController(true);
-        controller.getProcessList();
-
-        try {
-            controller.testRuntime();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        controller.openBrowser(LAUNCHER_URL);
-
-        Thread.sleep(20000);
-
-        controller.closeBrowser();
-    }
-    */
-
-
 }
--- src/com/cloudwhale/browsercontroller/SeleniumBrowserController.java
+++ src/com/cloudwhale/browsercontroller/SeleniumBrowserController.java
@@ -1,48 +1,149 @@
 package com.cloudwhale.browsercontroller;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.*;
+import org.openqa.selenium.NoSuchWindowException;
 import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class SeleniumBrowserController {
+class SeleniumBrowserController extends JFrame {
 
-    WebDriver driver;
-    boolean browserOpen;
+    private WebDriver driver;
+    private JLabel label;
 
-    public static final String CHROME_DRIVER_LOC = "C:\\chromedriver.exe";
-    public static final String GECKO_DRIVER_LOC = "C:\\geckodriver.exe";
-    public static final String EDGE_DRIVER_LOC = "C:\\MicrosoftWebDriver.exe"; //"C:\\msedgedriver.exe";
+    private String url;
 
-    public SeleniumBrowserController() {
-        /*System.setProperty("webdriver.firefox.marionette", GECKO_DRIVER_LOC);
+    private int width = 800;
+    private int height = 400;
+
+    private String currentTitle = "";
+    private String exitTitleCondition = "Exiting";
+
+    private static final String CHROME_DRIVER_LOC = "C:\\chromedriver.exe";
+    private static final String GECKO_DRIVER_LOC = "C:\\geckodriver.exe";
+    private static final String EDGE_DRIVER_LOC = "C:\\MicrosoftWebDriver.exe";
+
+    SeleniumBrowserController(String targetUrl) {
+        super("Cloudwhale Controller");
+
+        url = targetUrl;
+
+        // Configure window
+        java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        setBounds((screenSize.width - width) / 2, (screenSize.height - height) / 2, width, height);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+
+        // Attach closing event handle
+        addWindowListener(new java.awt.event.WindowAdapter() {
+
+            public void windowClosing(java.awt.event.WindowEvent evt) {
+                quitApplication();
+            }
+        });
+
+        // Open button
+        JButton jButton1 = new javax.swing.JButton();
+
+        // Create panel with label
+        label = new JLabel("");
+        JPanel panel = new JPanel(new FlowLayout());
+        panel.add(jButton1);
+
+        add(panel, BorderLayout.CENTER);
+        add(label, BorderLayout.SOUTH);
+
+        // Configure launch button
+        jButton1.setText("Open '"+ url + "'");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+
+                label.setText("Loading browser. Please wait..");
+
+                java.util.Timer t = new java.util.Timer();
+                t.schedule(new java.util.TimerTask() {
+
+                    @Override
+                    public void run() {
+                        openBrowserAndWait();
+                    }
+                }, 10);
+            }
+        });
+    }
+
+    private void openBrowserAndWait() {
+        System.setProperty("webdriver.gecko.driver", GECKO_DRIVER_LOC);
         driver = new FirefoxDriver();
-         */
-        /*System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_LOC);
-        driver = new ChromeDriver();
-         */
 
-        System.setProperty("webdriver.edge.driver", EDGE_DRIVER_LOC);
-        driver = new EdgeDriver();
+        // Maximize window
+        driver.manage().window().maximize();
+
+        String baseUrl = url;
+        driver.get(baseUrl);
+
+        java.util.Timer monitorTimer = new java.util.Timer();
+        monitorTimer.schedule(new java.util.TimerTask() {
+
+            @Override
+            public void run() {
+                while (true) {
+                    checkDriver();
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException ex) {
+                    }
+                }
+            }
+        }, 10);
     }
 
-    public void openUrl(String url) {
-        driver.get(url);
-        browserOpen = true;
+    private void checkDriver() {
+        if (driver == null) {
+            return;
+        }
+
+        boolean shouldExit = false;
+
+        try {
+            // Try to set the panels title to current browser title
+            currentTitle = driver.getTitle();
+            label.setText(currentTitle);
+
+            if (exitConditionIsMet()) {
+                shouldExit = true;
+            }
+
+        } catch (NoSuchWindowException e) {
+            System.out.println("Browser has been closed. Exiting Program");
+            shouldExit = true;
+        } catch (Exception e) {
+            System.out.println("Browser has been closed. Exiting Program");
+            shouldExit = true;
+        }
+
+        if (shouldExit) {
+            this.quitApplication();
+        }
     }
 
-    public boolean titleMatches(String expectedTitle) {
-        return browserOpen && driver.getTitle().contentEquals(expectedTitle);
+    private Boolean exitConditionIsMet() {
+        System.out.println("Current title '" + currentTitle + "' contains exit condition '" + exitTitleCondition + "'?\n" +
+                currentTitle.contains(exitTitleCondition));
+        return currentTitle.contains(exitTitleCondition);
     }
 
-    public void closeUrl() {
-        driver.close();
-        browserOpen = false;
-    }
+    private void quitApplication() {
+        // attempt to close gracefully
+        if (driver != null) {
+            try {
+                driver.quit();
+            } catch (Exception e) {
 
-    public void quit() {
-        driver.quit();
-        browserOpen = false;
+            }
+        }
+
+        System.exit(0);
     }
 }
